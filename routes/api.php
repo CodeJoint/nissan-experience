87157a07027fe1698cc06f39a2b68090769deaf5
John Falcon <falcon@walktopus.com>
2019-01-10 16:50:51 -0600

    First routes

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,8 @@ use Illuminate\Http\Request;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/robots', function (Request $request) {
+    return response("These aren't the droids you're looking for", 200);
 });
