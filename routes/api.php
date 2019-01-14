1e078a2b6048a99a15f31e8256dcc6a0a1974ac2
John Falcon <falcon@walktopus.com>
2019-01-14 17:12:31 -0600

    Basic routing for api requests

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,34 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-*/
-
-Route::get('/robots', function (Request $request) {
-    return response("These aren't the droids you're looking for", 200);
-});
+    /*   ___  ______ _____  ______            _   _
+	 *  / _ \ | ___ \_   _| | ___ \          | | (_)
+	 * / /_\ \| |_/ / | |   | |_/ /___  _   _| |_ _ _ __   __ _
+	 * |  _  ||  __/  | |   |    // _ \| | | | __| | '_ \ / _` |
+	 * | | | || |    _| |_  | |\ \ (_) | |_| | |_| | | | | (_| |
+	 * \_| |_/\_|    \___/  \_| \_\___/ \__,_|\__|_|_| |_|\__, |
+	 *                                                     __/ |
+	 *                                                     |___/
+	 * Author: John Falcon for Intus
+	 * Version: 1.2.1
+	 * Website: http://pixelton.xyz
+	 */
+    Route::group( [ 'domain' => config('api_base_url'), 'middleware' => ['cors'] ], function () {
+        
+        /**
+         * Test the connection to the api,
+         * THIS IS A  DUMMY ENDPOINT
+         */
+        Route::get('robots', function () {
+            return "These are not the droids you're looking for... \nv " . config('app.api_version');
+        });
+        
+        Route::post('log', function () {
+            return response(["success" => TRUE]);
+        });
+        
+        Route::post('action', function () {
+            return response(["success" => TRUE]);
+        });
+    
+    });
